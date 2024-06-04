0db5bb2598acf2ad6fb539182f53c8bfaebecaa0
TomoyaWashio <148823993+TomoyaWashio@users.noreply.github.com>
2024-06-04 11:34:22 +0900

    初回コミット

---
+++ wwwroot/download-helper.js
@@ -0,0 +1,20 @@
+﻿/**
+ * @param { byteArray: string, fileName: string, contentType: string} options
+ */
+function downloadFromByteArray(options) {
+    const blob = new Blob([options.byteArray], { type: options.contentType });
+    const url = URL.createObjectURL(blob);
+    downloadFromUrl({ url: url, fileName: options.fileName });
+    URL.revokeObjectURL(url);
+}
+
+/**
+ * @param { url: string, fileName?: string } options
+ */
+function downloadFromUrl(options) {
+    const anchorElement = document.createElement('a');
+    anchorElement.href = options.url;
+    anchorElement.download = options.fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+}
